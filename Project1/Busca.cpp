e6d3a6150bf2bec6d247ec2c99cc0df430e1d9c1
Roberto Júnior <robertodnevesjunior@ufmg.br>
2018-11-24 20:51:12 -0200

    Toda o mecanismo da busca para frases foi criado. Público somente uma função que retorna um MAP: <Palavras Buscadas, Set de livros que as contém>

--- Project1/Busca.cpp
+++ Project1/Busca.cpp
@@ -1,1 +1,83 @@
 #include "Busca.h"
+#include <string>
+#include <map>
+#include <algorithm>
+#include <fstream>
+
+using std::string;
+using std::map;
+
+void Busca::Inserir_Livro(string nome, string caminho)
+{
+	this->livros_[nome] = caminho;
+}
+
+void Busca::Remover_Livro(string nome)
+{
+	if (this->livros_.count(nome) > 0) {
+		map<string, string>::iterator livroIterator = this->livros_.find(nome);
+		this->livros_.erase(livroIterator);
+	}
+}
+
+map<string, std::set<string>> Busca::Buscar(string fraseDigitada)
+{
+	Busca_Frase(fraseDigitada);
+	return this->palavrasBuscadas_;
+}
+
+
+void Busca::Busca_Frase(string fraseBusca)
+{
+	this->palavrasBuscadas_.erase(this->palavrasBuscadas_.begin(), this->palavrasBuscadas_.end()); //Apagando informações da última busca
+	size_t posicao = 0;
+	string palavra;
+	string delimitador = " ";
+	while ((posicao = fraseBusca.find(delimitador)) != string::npos) {
+		palavra = fraseBusca.substr(0, posicao);
+		this->palavrasBuscadas_[palavra] = Busca_Nos_Arquivos(Arrumar_Palavra(palavra));
+		fraseBusca.erase(0, posicao + delimitador.length());
+	}
+}
+
+string Busca::Arrumar_Palavra(string palavra)
+{
+	char * palavraChar = (char*)palavra.c_str();
+	for (int i = 0; palavraChar[i] != NULL; i++) {
+		if (int(palavraChar[i] >= 65 && int(palavraChar[i]) <= 90)) //Maísculas transformadas em Minúsuclas
+			palavraChar[i] = palavraChar[i] + 32;
+		else if (int(palavraChar[i]) >= 97 && int(palavraChar[i]) <= 122) {}//Minúsculas -> Do nothing
+		else
+			palavraChar[i] = 33; //Tranforma todo outro tipo de caracter em ! para facilitar a remoção
+	}
+	palavra = (string)palavraChar;
+	palavra.erase(std::remove(palavra.begin(), palavra.end(), '!'), palavra.end());
+	return palavra;
+}
+
+std::set<string> Busca::Busca_Nos_Arquivos(string palavra)
+{
+	std::set<string> arquivosCorretos;
+	for (map < string, string>::iterator arquivoAtual = this->livros_.begin(); arquivoAtual != this->livros_.end(); arquivoAtual++) {
+		if (Varre_Arquivo((*arquivoAtual).second, palavra))
+			arquivosCorretos.insert((*arquivoAtual).first);
+	}
+	return arquivosCorretos;
+}
+
+bool Busca::Varre_Arquivo(string path, string palavraProcurada)
+{
+	string palavraArquivo;
+	std::ifstream arquivo(path);
+	if (arquivo.is_open()) {
+		while (!arquivo.eof()) {
+			std::getline(arquivo, palavraArquivo, ' ');
+			if (Arrumar_Palavra(palavraArquivo) == palavraProcurada) {
+				arquivo.close();
+				return true;
+			}
+		}
+		arquivo.close();
+	}
+	return false;
+}
--- Project1/Busca.h
+++ Project1/Busca.h
@@ -1,6 +1,26 @@
-#
+#ifndef BUSCA_H_
+#define BUSCA_H_
+
+#include <set>
+#include <string>
+#include <map>
+
+using std::string;
 
 class Busca {
+public:
+	void Inserir_Livro(string nome, string caminho);
+	void Remover_Livro(string nome);
+	map<string, std::set<string>> Buscar(string fraseDigitada);
+
+
 private:
-	set<string> livros;
+	std::map<string, string> livros_;
+	std::map<string, std::set<string>> palavrasBuscadas_;
+
+	void Busca_Frase(string fraseBusca);
+	string Arrumar_Palavra(string palavra);
+	std::set<string> Busca_Nos_Arquivos(string palavra);
+	bool Varre_Arquivo(string path, string palavra);
 };
+#endif
